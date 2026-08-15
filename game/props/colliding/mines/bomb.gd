45db78e0e0e94ab0fb0600a912de1974008c98b4
Luka Ludden <106929079+ludde017@users.noreply.github.com>
2026-08-15 16:49:35 -0500

    BOATS AND BOMBS

--- game/props/colliding/mines/bomb.gd
+++ game/props/colliding/mines/bomb.gd
@@ -1,11 +1,24 @@
-extends Node
+extends Node2D
+
+@export var horizontal_range: float = 700.0
+@export var horizontal_speed: float = 0.45
+@export var bob_height: float = 20.0
+@export var bob_speed: float = 1.3
+@export var rotation_speed: float = 0.3
+
+@onready var visual: Node2D = $Visual
+
+var origin: Vector2 = Vector2.ZERO
+var movement_time: float = 0.0
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	origin = position
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _physics_process(delta: float) -> void:
+	movement_time += delta
+	var horizontal_offset := sin(movement_time * horizontal_speed) * horizontal_range
+	var vertical_offset := sin(movement_time * bob_speed) * bob_height
+	position = origin + Vector2(horizontal_offset, vertical_offset)
+	visual.rotation += rotation_speed * delta
