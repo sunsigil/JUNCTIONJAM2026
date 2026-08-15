414aaa1779ecbfd7ed8403fd2e0f79eedf55cce0
Luka Ludden <106929079+ludde017@users.noreply.github.com>
2026-08-15 15:59:57 -0500

    Fish left to right sine movement pattern

--- game/fish/fish.gd
+++ game/fish/fish.gd
@@ -15,13 +15,23 @@ const INITIAL_POINTS: Array[Vector2] = [
 	Vector2(-64.0, 0.0),
 ]
 const SEGMENT_LENGTHS: Array[float] = [34.0, 34.0, 32.0, 28.0]
+const SHAPE_LENGTHS: Array[float] = [64.0, 62.0, 56.0]
 const SWIM_COLOR := Color("4bb8de")
 const NIBBLE_COLOR := Color("f2a65a")
 
 @export_category("PBD")
 @export_range(1, 24, 1) var solver_iterations: int = 8
 @export_range(0.0, 1.0, 0.01) var damping: float = 0.92
-@export var wake_strength: float = 90.0
+@export_range(0.0, 1.0, 0.01) var shape_stiffness: float = 0.55
+@export var wake_strength: float = 120.0
+
+@export_category("Swim Movement")
+@export var swim_range: Vector2 = Vector2(620.0, 220.0)
+@export var swim_speed: float = 0.55
+@export var nibble_swim_speed_scale: float = 0.3
+@export var side_to_side_duration: float = 7.0
+@export var vertical_swim_duration: float = 4.0
+@export var turn_speed: float = 4.0
 
 @export_category("Debug State Cycle")
 @export var auto_cycle_states: bool = true
@@ -45,10 +55,16 @@ var state: State = State.SWIM
 var state_origin: Vector2 = INITIAL_POINTS[0]
 var state_elapsed: float = 0.0
 var motion_time: float = 0.0
+var swim_center: Vector2 = Vector2.ZERO
+var swim_phase: float = 0.0
+var side_to_side_elapsed: float = 0.0
+var vertical_swim_phase: float = 0.0
+var vertical_swim_active: bool = false
 
 
 func _ready() -> void:
 	pieces = [head, upper_body, lower_body, body_end, tail]
+	swim_center = position
 	_reset_points()
 	_apply_debug_color()
 	_update_piece_transforms()
@@ -56,6 +72,7 @@ func _ready() -> void:
 
 
 func _physics_process(delta: float) -> void:
+	_update_swim_movement(delta)
 	if state == State.SWIM:
 		motion_time += delta
 	state_elapsed += delta
@@ -98,21 +115,79 @@ func _integrate_points(delta: float) -> void:
 
 func _solve_distance_constraints() -> void:
 	var head_target := points[0]
+	var shape_iteration_stiffness := _get_shape_iteration_stiffness()
 	for _iteration in range(solver_iterations):
 		points[0] = head_target
 		for index in range(SEGMENT_LENGTHS.size()):
-			var next_index := index + 1
-			var offset := points[next_index] - points[index]
-			var distance := offset.length()
-			if distance <= 0.0001:
-				continue
+			_solve_constraint(index, index + 1, SEGMENT_LENGTHS[index], 1.0)
+		for index in range(SHAPE_LENGTHS.size()):
+			_solve_constraint(index, index + 2, SHAPE_LENGTHS[index], shape_iteration_stiffness)
+	points[0] = head_target
 
-			var correction := offset * ((distance - SEGMENT_LENGTHS[index]) / distance)
-			if index == 0:
-				points[next_index] -= correction
-			else:
-				points[index] += correction * 0.5
-				points[next_index] -= correction * 0.5
+
+func _get_shape_iteration_stiffness() -> float:
+	var safe_iterations := maxi(solver_iterations, 1)
+	return 1.0 - pow(1.0 - shape_stiffness, 1.0 / float(safe_iterations))
+
+
+func _solve_constraint(
+		first_index: int,
+		second_index: int,
+		rest_length: float,
+		stiffness: float
+) -> void:
+	var offset := points[second_index] - points[first_index]
+	var distance := offset.length()
+	if distance <= 0.0001:
+		return
+
+	var correction := offset * ((distance - rest_length) / distance) * stiffness
+	if first_index == 0:
+		points[second_index] -= correction
+	else:
+		points[first_index] += correction * 0.5
+		points[second_index] -= correction * 0.5
+
+
+func _update_swim_movement(delta: float) -> void:
+	var speed_scale := nibble_swim_speed_scale if state == State.NIBBLE else 1.0
+	var movement_delta := delta * speed_scale
+	swim_phase = fmod(swim_phase + movement_delta * swim_speed, TAU)
+
+	if vertical_swim_active:
+		var safe_vertical_duration := maxf(vertical_swim_duration, 0.001)
+		vertical_swim_phase += movement_delta * TAU / safe_vertical_duration
+		if vertical_swim_phase >= TAU:
+			vertical_swim_active = false
+			vertical_swim_phase = 0.0
+			side_to_side_elapsed = 0.0
+	else:
+		side_to_side_elapsed += movement_delta
+		if side_to_side_elapsed >= side_to_side_duration:
+			vertical_swim_active = true
+			vertical_swim_phase = 0.0
+
+	position = swim_center + _get_swim_offset()
+	var tangent := _get_swim_tangent()
+	if not tangent.is_zero_approx():
+		var turn_weight := clampf(turn_speed * delta, 0.0, 1.0)
+		rotation = lerp_angle(rotation, tangent.angle(), turn_weight)
+
+
+func _get_swim_offset() -> Vector2:
+	var vertical_offset := 0.0
+	if vertical_swim_active:
+		vertical_offset = sin(vertical_swim_phase) * swim_range.y
+	return Vector2(sin(swim_phase) * swim_range.x, vertical_offset)
+
+
+func _get_swim_tangent() -> Vector2:
+	var horizontal_velocity := cos(swim_phase) * swim_range.x * swim_speed
+	var vertical_velocity := 0.0
+	if vertical_swim_active:
+		var safe_vertical_duration := maxf(vertical_swim_duration, 0.001)
+		vertical_velocity = cos(vertical_swim_phase) * swim_range.y * TAU / safe_vertical_duration
+	return Vector2(horizontal_velocity, vertical_velocity)
 
 
 func _get_head_target() -> Vector2:
